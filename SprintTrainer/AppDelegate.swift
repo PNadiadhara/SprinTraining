e7caadee72fd3c7524f33e70f1deadd6ace5bb28
PN <priteshrna@gmail.com>
2020-08-23 16:26:04 -0400

    info plist and app delegate updated to request use info to update

--- SprintTrainer/AppDelegate.swift
+++ SprintTrainer/AppDelegate.swift
@@ -16,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        // Ask user for location permissions]
+        let locationManager = LocationManager.shared
+        locationManager.requestWhenInUseAuthorization()
         return true
     }
 
---
+++ SprintTrainer/SupportingFiles/LocationManager.swift
@@ -0,0 +1,16 @@
+//
+//  LocationManager.swift
+//  SprintTrainer
+//
+//  Created by Pritesh Nadiadhara on 8/23/20.
+//  Copyright © 2020 PriteshN. All rights reserved.
+//
+
+import CoreLocation
+
+class LocationManager {
+    static let shared = CLLocationManager()
+    
+    private init() {}
+    
+}
