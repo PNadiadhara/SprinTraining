7f5d880f70190b44633c6380abf508cb046d1bed
PN <priteshrna@gmail.com>
2020-08-23 18:25:57 -0400

    Coredata to save location objects

--- SprintTrainer/ViewControllers/TrainingViewController.swift
+++ SprintTrainer/ViewControllers/TrainingViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import MapKit
+import CoreLocation
 
 class TrainingViewController: UIViewController {
     
@@ -18,33 +18,100 @@ class TrainingViewController: UIViewController {
     
     @IBOutlet weak var stopButton: UIButton!
     
-   
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var paceLabel: UILabel!
+    
     
     private var run: Run?
+    
+    //used to start and stop location services
+    private let locationManager = LocationManager.shared
+    // keeps track of points ran as CLLocation objects
+    private var locationList: [CLLocation] = []
+    
+    private var seconds = 0
+    private var timer: Timer?
+    private var distance = Measurement(value: 0, unit: UnitLength.meters)
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-//        view.backgroundColor = UIColor.green
-        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer?.invalidate()
+        locationManager.stopUpdatingLocation()
     }
     
     private func startRun(){
         infoStackView.isHidden = false
         startButton.isHidden = true
         stopButton.isHidden = false
+        
+        seconds = 0
+        distance = Measurement(value: 0, unit: UnitLength.meters)
+        locationList.removeAll()
+        updateDisplay()
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {_ in
+            self.updateSeconds()
+        }
+        startLocationUpdates()
+    }
+    
+    
+    private func saveRun() {
+        let newRun = Run(context: CoreDataStack.context)
+        newRun.distance = distance.value
+        newRun.duration = Int16(seconds)
+        newRun.timestamp = Date()
+        
+        for location in locationList {
+            let locationObject = Location(context: CoreDataStack.context)
+            locationObject.timestamp = location.timestamp
+            locationObject.latitude = location.coordinate.latitude
+            locationObject.longitude = location.coordinate.longitude
+            newRun.addToLocations(locationObject)
+   
+        }
+        CoreDataStack.saveContext()
+        
+        run = newRun
     }
 
     private func stopRun(){
         infoStackView.isHidden = true
         stopButton.isHidden = true
         startButton.isHidden = false
+        
+        locationManager.stopUpdatingLocation()
+    }
+   
+    func updateDisplay(){
+        let formattedDistance = FormatDisplay.distance(distance)
+        let formattedTime = FormatDisplay.time(seconds)
+        // update outputUnit to be modular for future updates
+        let formatedPace = FormatDisplay.pace(distance: distance, seconds: seconds, outputUnit: .minutesPerMile)
+        
+        
+        distanceLabel.text = "Distance: \(formattedDistance)"
+        timeLabel.text = "Time: \(formattedTime)"
+        paceLabel.text = "Pace: \(formatedPace)"
+    }
+    
+    
+    func updateSeconds(){
+        seconds += 1
+        updateDisplay()
     }
   
     @IBAction func startTapped(_ sender: Any) {
         startRun()
-        print("Start Pressed")
+        
     }
     
     @IBAction func stopTapped(_ sender: Any) {
@@ -54,6 +121,7 @@ class TrainingViewController: UIViewController {
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         alertController.addAction(UIAlertAction(title: "Save", style: .default, handler: { (UIAlertAction) in
             self.stopRun()
+            self.saveRun()
             self.performSegue(withIdentifier: .details, sender: nil)
         }))
         alertController.addAction(UIAlertAction(title: "Discard", style: .destructive, handler: { (UIAlertAction) in
@@ -63,6 +131,13 @@ class TrainingViewController: UIViewController {
         
         present(alertController, animated: true)
         
+    }
+    
+    private func startLocationUpdates(){
+        locationManager.delegate = self
+        locationManager.activityType = .fitness
+        locationManager.distanceFilter = 10
+        locationManager.startUpdatingLocation()
     }
     
 }
@@ -77,6 +152,25 @@ extension TrainingViewController : SegueHandlerType {
         case .details:
             let destination = segue.destination as! RunDetailsViewController
             destination.run = run
+        }
+    }
+}
+
+
+extension TrainingViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for newLocation in locations {
+            let howRecent = newLocation.timestamp.timeIntervalSinceNow
+            guard newLocation.horizontalAccuracy < 20 && abs(howRecent) < 10 else {
+                continue
+            }
+            
+            if let lastLocation = locationList.last {
+                let delta = newLocation.distance(from: lastLocation)
+                distance = distance + Measurement(value: delta, unit: UnitLength.meters)
+            }
+            
+            locationList.append(newLocation)
         }
     }
 }
