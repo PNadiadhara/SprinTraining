33e8430a1d208c7226d593bd4da4111f383885cf
PN <priteshrna@gmail.com>
2020-08-24 11:04:59 -0400

    set mapview delegate to self to show polyline overlay on map

---
+++ SprintTrainer/SupportingFiles/PolylineColor.swift
@@ -0,0 +1,15 @@
+//
+//  PolylineColor.swift
+//  SprintTrainer
+//
+//  Created by Pritesh Nadiadhara on 8/24/20.
+//  Copyright © 2020 PriteshN. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MulticolorPolyline: MKPolyline {
+  var color = UIColor.black
+}
+
